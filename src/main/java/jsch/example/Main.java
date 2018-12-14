aa861509807b38009a02fbe15706b82d659b7da0
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-12-14 16:04:42 +0100

    init

---
+++ src/main/java/jsch/example/Main.java
@@ -0,0 +1,19 @@
+package jsch.example;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class Main {
+
+	public static final void main(String[] args) throws JSchException {
+		JSch jsch = new JSch();
+		jsch.addIdentity(System.getProperty("user.home") + "/.ssh/id_rsa", "XXXXXXXXXXX");
+		Session session = jsch.getSession("git", "github.com", 22);
+        java.util.Properties config = new java.util.Properties(); 
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+		session.connect();
+	}
+
+}
